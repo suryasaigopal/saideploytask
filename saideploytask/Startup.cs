139cdd92f38e8c7cfc056455f477bb8679daf7be
Sai <Sai@AZTaskVM1>
2019-03-03 04:18:52 +0000

    Add project files.

---
+++ saideploytask/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(saideploytask.Startup))]
+namespace saideploytask
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
